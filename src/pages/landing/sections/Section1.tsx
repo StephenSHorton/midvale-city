e0d7dc1a0369de0e81ab510517bd46a870174ae5
StephenSHorton <4stephenhorton@gmail.com>
2021-10-20 17:23:32 -0600

    removed video

--- src/pages/landing/sections/Section1.tsx
+++ src/pages/landing/sections/Section1.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Section1: FC = () => {
 	return (
-		<div className="bg-mountains w-full md:h-[1271px] flex flex-col items-center text-black">
+		<div className="flex flex-col items-center w-full text-black bg-mountains">
 			<div className="place-self-start">
 				<div className="self-start w-0 font-extrabold text-white uppercase scale-75 text-7xl sm:scale-100">
 					<div className="w-[275px] p-2 text-right bg-midvale-blue border-8 border-midvale-blue">
@@ -21,21 +21,10 @@ const Section1: FC = () => {
 					</div>
 				</div>
 			</div>
-			<h2 className="p-6 my-6 text-2xl font-light text-center bg-white shadow-2xl md:text-4xl bg-opacity-20">
+			<h2 className="p-6 my-32 text-2xl font-light text-center bg-white shadow-2xl md:text-4xl bg-opacity-20">
 				<strong>Ranked-Choice Voting</strong> allows voters to vote
 				their preference
 			</h2>
-			<p></p>
-			<div className="p-6 mb-10 bg-white shadow-2xl bg-opacity-20 md:mb-0">
-				<iframe
-					className="w-[400px] h-[245px] xl:w-[1280px] xl:h-[720px]"
-					src="https://www.youtube.com/embed/TIVFxAB3LV8"
-					title="YouTube video player"
-					frameBorder="0"
-					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-					allowFullScreen
-				></iframe>
-			</div>
 		</div>
 	);
 };
